16e1227d8f84fb53eff0e8fc11f7fb697f5f3874
wanjiiru <liz240@live.com>
2018-05-03 12:13:03 -0500

    add style and functions to index and js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+var output = "";
+
+function getResponse(play){
+    if (play % 5 ===0 && play % 3 === 0) {
+        output = "Ping pong "
+    } else if (play % 5 === 0) {
+        output = "Pong"
+    } else if (play % 3 === 0) {
+        output = "Ping"
+    } else {
+        output = play;}
+    return output;
+}
+
+$(document).ready(function () {
+    $("form#get-results").submit(function (event) {
+        event.preventDefault();
+        var num = $("#user-input").val();
+        console.log(num)
+        for(var counter =1;counter <=num; counter ++ ) {
+            var result = getResponse(counter)
+            console.log(result)
+            $("ul#list-1").append("<li class='myList'>"+result+"</li>")
+        }
+        $("#user-input").val("");
+    })
+
+});
